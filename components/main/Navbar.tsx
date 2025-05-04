a37a0b52575ff54690353171a9bb7dc8eb71a1d5
ko5yak <47902870+ko5yak@users.noreply.github.com>
2025-05-04 14:21:30 +0300

    Update Navbar.tsx
    Centered Navbar links

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -5,10 +5,11 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+      <div className="w-full h-full flex items-center justify-center relative">
+        {/* Logo - Positioned on the left */}
         <a
           href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
+          className="absolute left-5 h-auto w-auto flex flex-row items-center"
         >
           <Image
             src="/NavLogo.png"
@@ -17,24 +18,22 @@ const Navbar = () => {
             height={70}
             className="cursor-pointer hover:animate-slowspin"
           />
-
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
             Stealth-AI
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
-              About
-            </a>
-            <a href="#skills" className="cursor-pointer">
-              Tech
-            </a>
-            <a href="#projects" className="cursor-pointer">
-              Projects
-            </a>
-          </div>
+        {/* Centered Navigation Links */}
+        <div className="flex items-center border border-[#7042f861] bg-[#0300145e] px-[20px] py-[10px] rounded-full text-gray-200">
+          <a href="#about-me" className="cursor-pointer px-4">
+            About
+          </a>
+          <a href="#skills" className="cursor-pointer px-4">
+            Tech
+          </a>
+          <a href="#projects" className="cursor-pointer px-4">
+            Projects
+          </a>
         </div>
       </div>
     </div>
